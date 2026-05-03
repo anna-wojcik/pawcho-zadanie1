18ca1327623f81bd901b30a91b955c84f2394439
Anna Wójcik <aniawojcik894@gmail.com>
2026-05-03 23:29:06 +0200

    Replace built-in HTTP server with Netcat for serving index.html

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 #Wygenerowanie logu o dacie, autorze i porcie serwera
 echo "[$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Autor: Anna Wójcik, Serwer uruchomiony na porcie: 3000"
 
-# Uruchomienie wbudowanego w Alpine lekkiego serwera HTTP
-exec busybox httpd -f -p 3000 -h /app
+# Uruchomienie serwera Netcat w pętli, który będzie nasłuchiwał na porcie 3000 i zwracał zawartość pliku index.html
+while true; do 
+  echo -e "HTTP/1.1 200 OK\n\n$(cat /app/index.html)" | nc -l -p 3000
+done
