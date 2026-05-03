bb372c57943d44b3925ec70482c7c624fa8b178c
Anna Wójcik <aniawojcik894@gmail.com>
2026-05-03 23:12:26 +0200

    Remove busybox-extras installation from Dockerfile and update run.sh to use busybox for HTTP server

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 #Wygenerowanie logu o dacie, autorze i porcie serwera
 echo "[$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Autor: Anna Wójcik, Serwer uruchomiony na porcie: 3000"
 
-# Uruchomienie wbudowanego w Apline lekkiego serwera HTTP
-exec httpd -f -p 3000 -h /app
+# Uruchomienie wbudowanego w Alpine lekkiego serwera HTTP
+exec busybox httpd -f -p 3000 -h /app
